944bcfc841b6de6fcb072d0667a3a815ed331387
Denys Lopez <denys.lopez.pinate@everis.com>
2019-11-20 11:33:26 -0400

    Primer commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO clientes (nombre, apellido, create_at, email) VALUES ('Denys', 'Lopez', '2017-12-11', 'denys@gmail.com');
+INSERT INTO clientes (nombre, apellido, create_at, email) VALUES ('Yusmarys', 'Guatarama', '2018-10-11', 'yusmarys@gmail.com');
+INSERT INTO clientes (nombre, apellido, create_at, email) VALUES ('Darihanna', 'Lopez', '2019-05-04', 'darihanna@gmail.com');
+INSERT INTO clientes (nombre, apellido, create_at, email) VALUES ('Maria', 'Piñate', '2010-01-20', 'maria@gmail.com');
+INSERT INTO clientes (nombre, apellido, create_at, email) VALUES ('Deivis', 'Guatarama', '2018-11-19', 'deivis@gmail.com');
+INSERT INTO clientes (nombre, apellido, create_at, email) VALUES ('Genessis', 'Guatarama', '2018-11-01', 'genesis@gmail.com');
+INSERT INTO clientes (nombre, apellido, create_at, email) VALUES ('Dania', 'Delgado', '2019-06-29', 'dania@gmail.com');
+INSERT INTO clientes (nombre, apellido, create_at, email) VALUES ('Pepe', 'Pepito', '2000-12-31', 'pepe@gmail.com');
+INSERT INTO clientes (nombre, apellido, create_at, email) VALUES ('Pedro', 'Marquez', '2005-08-22', 'pedro@gmail.com');
+INSERT INTO clientes (nombre, apellido, create_at, email) VALUES ('Juan', 'Perez', '2009-10-05', 'juan@gmail.com');
+INSERT INTO clientes (nombre, apellido, create_at, email) VALUES ('Petra', 'Josefa', '2011-10-20', 'petra@gmail.com');
